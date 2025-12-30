921050234f91a85fbfb5d87dc362e96aab3b87b8
Akhilvarma12 <akhilvarma2004@gmail.com>
2025-12-30 20:04:05 +0530

    added changes to deploy the frontend

--- client/src/api/articles.js
+++ client/src/api/articles.js
@@ -1,5 +1,7 @@
-const API_BASE_URL = "http://localhost:5000/api/articles";
-
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000/api/articles";
+  
 export async function fetchArticles() {
   const res = await fetch(API_BASE_URL);
   if (!res.ok) throw new Error("Failed to fetch articles");
